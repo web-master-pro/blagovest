a89b4adbda1b25f32c1559b17bc20bae530ebdf8
Alexey Dobrov <mail@web-master-pro.ru>
2016-08-05 08:53:49 +0300

    Адаптивный по высоте первый экран на главной + Переключение контента филиалов на странице контактов

--- src/blocks/map/map.js
+++ src/blocks/map/map.js
@@ -1,9 +1,74 @@
 $(document).ready(function(){
 
+    var latitude = 59.830676,
+        longitude = 30.385065,
+        map,
+        marker,
+        markerImage = 'assets/img/map-marker.png';
+        styleArray = [{featureType: "all"}];
+
+    function getCoords(){
+        var addressEl = $(".baloon__branch.active .baloon__address");
+        if (addressEl.length > 0){
+            latitude = addressEl.attr("data-latitude"),
+            longitude = addressEl.attr("data-longitude");
+        };
+        // console.log(latitude + "; " + longitude);
+    };
+
+    getCoords();
+
+    function initializeMap() {
+        var width = window.innerWidth
+            || document.documentElement.clientWidth
+            || document.body.clientWidth,
+            longitudeCenter;
+        if (width >= 1152) {
+            longitudeCenter = longitude - 0.0035;
+        } else {
+            longitudeCenter = longitude;
+        };
+        var mapOptions = {
+            zoom: 16,
+            center: new google.maps.LatLng(latitude, longitudeCenter),
+            scrollwheel: false,
+            draggable: false,
+            zoomControl: true,
+            styles: styleArray,
+        };
+
+        map = new google.maps.Map(document.getElementById('map-holder'), mapOptions);
+        marker = new google.maps.Marker({
+            position: new google.maps.LatLng(latitude,longitude),
+            map: map,
+            icon: markerImage
+        });
+    }
+
+    google.maps.event.addDomListener(window, 'load', initializeMap);
+
+    $(window).on('hashchange', function() {
+        if (window.location.hash) {
+            if ($(".map").length > 0) {
+                getCoords();
+                setTimeout(function() {
+                    google.maps.event.trigger(map, 'resize');
+                }, 300);
+                initializeMap();
+            };
+
+        };
+    });
+
     $(".js-map-button").click(function(e){
         e.preventDefault();
         $(".map__map").addClass("on");
-        $(this).fadeOut(100);
+        $(this).fadeOut(300);
+        setTimeout(function() {
+            initializeMap();
+            google.maps.event.trigger(map, 'resize');
+        }, 1000);
     });
+
 
 });
--- src/blocks/menu-category/menu-category.js
+++ src/blocks/menu-category/menu-category.js
@@ -1,5 +1,16 @@
 $(document).ready(function(){
 
+    function updateMenuCategoryMaxHeight(){
+        var maxItems = 5,
+            maxHeight = 0;
+        if ($(".menu-category__item").length >= maxItems) {
+            for (var i=0; i < maxItems; i++) {
+                maxHeight = maxHeight + $(".menu-category__item").eq(i).outerHeight();
+            };
+            $(".menu-category__menu").css({"max-height": maxHeight});
+        };
+    };
+
     $(".menu-category__button").click(function(){
         var dropdown = $(".menu-category__menu"),
             menu = $(this).parent(".menu-category");
@@ -9,8 +20,61 @@ $(document).ready(function(){
         } else {
             menu.addClass("on");
             dropdown.addClass("on").slideDown(300);
+            updateMenuCategoryMaxHeight();
         }
     });
+
+    // function switchBranch() {
+    //     if (window.location.hash) {
+    //         var hash = window.location.hash.substring(1);
+    //         if ($("#contacts-menu-branch").length > 0) {
+    //             console.log(hash);
+    //         }
+    //     }
+    // };
+
+    // function GetLocationByAddress(address) {
+    //     var geocoder = new google.maps.Geocoder();
+    //     console.log(address);
+    //     geocoder.geocode({ 'address': address }, function (results, status) {
+    //         if (status == google.maps.GeocoderStatus.OK) {
+    //             var latitude = results[0].geometry.location.lat();
+    //             var longitude = results[0].geometry.location.lng();
+    //             console.log("Latitude: " + latitude + "\nLongitude: " + longitude);
+    //         } else {
+    //             console.log("Request failed.")
+    //         }
+    //     });
+    // };
+
+    function switchBranch(index){
+        var branchCaption = $(".menu-category__link").eq(index).text();
+        $(".choose-branch__title").text(branchCaption);
+        $(".menu-category__current").text(branchCaption);
+        $(".menu-category__img").removeClass("active")
+        $(".menu-category__img").eq(index).addClass("active")
+        $(".baloon__branch").removeClass("active");
+        $(".baloon__branch").eq(index).addClass("active");
+        $(".team__branch").removeClass("active");
+        $(".team__branch").eq(index).addClass("active");
+
+        // var address = $(".baloon__branch.active .baloon__address").text();
+        // GetLocationByAddress(address);
+    };
+
+    $("#contacts-menu-branch .menu-category__link").click(function(e){
+        // e.preventDefault();
+        var links = $("#contacts-menu-branch .menu-category__link"),
+            index = $(links).index($(this)),
+            dropdown = $(".menu-category__menu"),
+            menu = $(".menu-category");
+        if (menu.hasClass("on")) {
+            menu.removeClass("on");
+            dropdown.slideUp(300);
+        };
+        switchBranch(index);
+    });
+
 
 
 
