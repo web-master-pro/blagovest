cb2b973994c43123ac176a3dffaaaacbc6069c0b
Alexey Dobrov <mail@web-master-pro.ru>
2016-09-04 13:52:03 +0300

    Фикс проблемы с мобильным меню на Safari

--- src/blocks/menu-side/menu-side.js
+++ src/blocks/menu-side/menu-side.js
@@ -1,14 +1,22 @@
 $(document).ready(function(){
     $(".menu-side__title-link").click(function(event){
-        event.preventDefault()
+        event.preventDefault();
+        var menu = $(this).parent().next(".menu-side__menu");
         if ($(this).hasClass("expanded")) {
             $(this).removeClass("expanded");
-            $(this).parent().next(".menu-side__menu").slideUp(300);
+            // $(this).parent().next(".menu-side__menu").removeClass("expanded");
+            menu.slideUp("fast",function(){
+                menu.removeClass("expanded").slideDown(0);
+            })
+
         } else {
             $(".menu-side__title-link").removeClass("expanded");
+            $(".menu-side__menu").removeClass("expanded");
             $(this).addClass("expanded");
-            $(".menu-side__menu").slideUp(300);
-            $(this).parent().next(".menu-side__menu").slideDown(300);
+            // $(this).parent().next(".menu-side__menu").addClass("expanded");
+            menu.slideUp(0,function(){
+                menu.addClass("expanded").slideDown("fast");
+            })
         }
         return true;
     })
